3dceee101c720100575ace09475402807672b801
claw1405 <46711240+claw1405@users.noreply.github.com>
2025-10-28 03:14:11 +0000

    Saving a graph directly to a file

--- data_visualization.py
+++ data_visualization.py
@@ -11,16 +11,19 @@ fig, ax = plt.subplots()
 #ax.scatter(x_values, y_values, color=(0, 0.8, 0), s=2)
 
 #This shows how to use gradients in plyplot
-ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Reds, s=2)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Reds, s=10)
 
 #ax.plot(input_values, squares, linewidth=3)
+
+# You can save the fig straight to a file 
+plt.savefig('squares_plot.png', bbox_inches='tight')
 
 # Set titles
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of values", fontsize=14)
 
-ax.axis([0, 1100, 0, 1_100_000])
+ax.axis([0, 1100, 0, 1_000_000])
 ax.ticklabel_format(style='plain')
 
 ax.tick_params(labelsize=14)
