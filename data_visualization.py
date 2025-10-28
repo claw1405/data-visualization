f312018c4f6da1d38054e712058bf7b3ba54cec8
claw1405 <46711240+claw1405@users.noreply.github.com>
2025-10-28 02:58:11 +0000

    Initial commit

---
+++ data_visualization.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+input_values = [1, 2, 3, 4, 5]
+squares = [1, 4, 9 , 16, 25]
+
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
+
+plt.style.use('ggplot')
+fig, ax = plt.subplots()
+#ax.scatter(x_values, y_values, color=(0, 0.8, 0), s=2)
+
+#This shows how to use gradients in plyplot
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Reds, s=2)
+
+#ax.plot(input_values, squares, linewidth=3)
+
+# Set titles
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of values", fontsize=14)
+
+ax.axis([0, 1100, 0, 1_100_000])
+ax.ticklabel_format(style='plain')
+
+ax.tick_params(labelsize=14)
+
+plt.show()
